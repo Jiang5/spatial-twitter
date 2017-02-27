cf54735c99840f7b2c5e2aabf0ca6934c27f448e
Jiang5 <wujiangdgg@gmail.com>
2017-02-27 01:12:01 -0500

    Create spatial_twitter.r

---
+++ spatial_twitter.r
@@ -0,0 +1,31 @@
+library(twitteR)
+library(ggmap)
+library(ggplot2)
+library(readr)
+#get Twitter token at development website
+setup_twitter_oauth()
+
+#tweets collecting section
+keyword = "#cool"
+tweets = searchTwitter(keyword, n = 100)
+tweets = twListToDF(tweets)
+tweetsUsers = twListToDF(lookupUsers(tweets$screenName))
+withLocation = subset(tweetsUsers, location != "" , location)
+geoCodes = geocode(withLocation$location)
+geoCodesTotalNoNA = na.omit(geoCodes)
+
+#grouping tweets by state geo within 5 degree 
+for(i in 1 : 52) {
+    v1 = abs(geoCodesTotalNoNA$lon - as.numeric(state_geo[i,1])) <= 5 & abs(geoCodesTotalNoNA$lat - as.numeric(state_geo[i,2])) <= 5
+    v2 = length(which(v1))
+    state_geo[i,4] = v2   
+}
+#eliminate Alaska and Hawaii for denser plot, sorry Alaska and Hawaii
+land = state_geo[state_geo$state != c("Alaska","Hawaii"), ]
+
+#plot section
+#when query beyond the API limit, or use mapCenter = as.numeric(geocode("United States"))
+mapCenter = c(-98, 40)
+#inspred by Domino,https://blog.dominodatalab.com/geographic-visualization-with-rs-ggmaps/
+USAMap = ggmap(get_googlemap(center = mapCenter, scale = 2, zoom = 4), extent="normal")
+USAMap + geom_point(aes(x = lon, y = lat), data = land, col = "red", alpha = 0.5, size = as.numeric(land$num)*0.05) 
